f910cb1afffa814cf4e24e06b99a6330a7522ed2
KatiaCursi <cursikatia@gmail.com>
2024-12-09 17:31:53 -0300

    criação do serviço

--- src/main/java/com/agenda/agenda/services/agendaService.java
+++ src/main/java/com/agenda/agenda/services/agendaService.java
@@ -1,8 +1,22 @@
 package com.agenda.agenda.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.agenda.agenda.dtos.agendaResponse;
+import com.agenda.agenda.mappers.agendaMapper;
+import com.agenda.agenda.repositories.agendaRepository;
 
 @Service
 public class agendaService {
     
+    @Autowired
+    private agendaRepository repository;
+
+    public List<agendaResponse> getAllagenda() {
+return repository.findAll().stream().map(p -> agendaMapper.toDTO(p)).collect(Collectors.toList());
+    }
 }
