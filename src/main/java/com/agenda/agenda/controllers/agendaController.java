ec9d33a131f8d9fddeb80d7d434674aa8855824f
KatiaCursi <cursikatia@gmail.com>
2024-12-09 17:50:10 -0300

    criação da controller

---
+++ src/main/java/com/agenda/agenda/controllers/agendaController.java
@@ -0,0 +1,25 @@
+package com.agenda.agenda.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agenda.agenda.dtos.agendaResponse;
+import com.agenda.agenda.services.agendaService;
+
+@RestController
+@RequestMapping("agenda")
+public class agendaController {
+
+    @Autowired
+    private agendaService service;
+    
+    @GetMapping
+    public ResponseEntity<List<agendaResponse>> getagenda(){
+        return ResponseEntity.ok(service.getAllagenda());  
+    }
+}
